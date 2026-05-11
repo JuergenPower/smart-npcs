358fbf4a6d5d8cc5419a1f1c8b78bae359091e69
Juergen Hoedl <juergen_hoedl@web.de>
2026-05-11 14:23:16 +0200

    Initial setup: phaser + vite + typescript

---
+++ README.md
@@ -0,0 +1,11 @@
+# Phaser Vite TypeScript Template
+
+This is a Phaser project to test more interesting NPC interactions utilizing AI.
+
+## Available Commands
+
+| Command | Description |
+|---------|-------------|
+| `npm install` | Install project dependencies |
+| `npm run dev` | Launch a development web server |
+| `npm run build` | Create a production build in the `dist` folder |
---
+++ src/game/main.ts
@@ -0,0 +1,22 @@
+import { Game as MainGame } from './scenes/Game';
+import { AUTO, Game } from 'phaser';
+
+
+const config: Phaser.Types.Core.GameConfig = {
+    type: AUTO,
+    width: 1024,
+    height: 768,
+    parent: 'game-container',
+    backgroundColor: '#028af8',
+    scene: [
+        MainGame
+    ]
+};
+
+const StartGame = (parent: string) => {
+
+    return new Game({ ...config, parent });
+
+}
+
+export default StartGame;
